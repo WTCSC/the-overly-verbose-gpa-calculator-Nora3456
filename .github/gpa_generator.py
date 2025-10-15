ec2fa12ec2a5bece5991d151af3b8c6ce193131b
Nora Rice <nora.rice09@gmail.com>
2025-10-15 14:44:33 -0600

    adding validate

--- .github/gpa_generator.py
+++ .github/gpa_generator.py
@@ -1,12 +1,20 @@
 print ("Welcome to the GPA calculator!")
 
-num_of_classes = int(input(f"How  many classes do you have?:"))
+num_of_classes = input(f"How many classes do you have?:")
+
+def validate_num_of_classes(input):
+     if num_of_classes != '1' '2' '3' '4' '5' '6' '7' '8' '9' '10':
+          return False
+     
+def grades_to_GPA(dividedby):
+    class_1 + class_2 + class_3 + class_4 + class_5 + class_6 / dividedby
 
 
-grades =[(0.0, 4.0)]
+while validate_num_of_classes(input) == True: 
+    print ("Lets be realistic in the number of classes you are taking, make sure to just enter the number of classes you are taking, no extra characters to avoid confusion. Try again. ")
 
 
-if num_of_classes == 5:
+if num_of_classes == '5':
 
     class_1 = float(input("What is your grade in your 1st class?:"))
     class_2 = float(input("What is your grade in your 2nd class?:"))
@@ -14,7 +22,9 @@ if num_of_classes == 5:
     class_4 = float(input("What is your grade in your 4th class?:"))
     class_5 = float(input("What is your grade in your 5th class?:"))
 
-if num_of_classes == 6:
+    grades_to_GPA(class_2, class_3, class_4, class_5, 5)
+
+if num_of_classes == '6':
 
     class_1 = float(input("What is your grade in your 1st class?:"))
     class_2 = float(input("What is your grade in your 2nd class?:"))
@@ -24,3 +34,7 @@ if num_of_classes == 6:
     class_6 = float(input("What is your grade in your 6th class?:"))
 
 print ("cal..cu..la...ting... still... calculating... alright, done! Based on {num_of_classes} classes, your GPA is currently: #input GPA#")
+
+grades =[(0.0, 4.0)]
+
+grades = 10
